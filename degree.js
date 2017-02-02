2bb002a90844bcfb76531705c56eb18a923bb8ea
Rory O'Connor <7roconnor@stpatrick.org>
2017-02-02 13:59:32 -0600

    Add files via upload

---
+++ degree.js
@@ -0,0 +1,39 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [1,1,0,1,0],
+    [1,0,1,0,0],
+    [0,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,0,1,0]
+]);
+//Code here
+function degree(mtrx,v)
+{
+  let sum = 0;
+  let degreeList = mtrx(v-1);
+
+  for(let i = 0; i < degreeList.length; i++)
+  {
+    sum += degreeList[i];
+  }
+  return sum;
+}
+
+function degreecentrality(mtrx, v)
+{
+  let neighbors = mtrx(v-1).length;
+  let mtrxDegree = (degree(mtrx, v) / neighbors);
+  return mtrxDegree;
+}
+
+function king()
+{
+  
+}
+
+//Challenge
+//degree(A, 1);
+//Run test code with ctrl+shift+p  then type in "script"
+//console.log(degree(A,4));
+console.log(degreecentrality(A, 2));
